c2149f96e12cf83c4969dc0947a1b30e245be53a
Bill Xue <billxue9141@hotmail.com>
2017-10-19 14:29:47 -0700

    working on job#7
    Signed-off-by: Bill Xue <billxue9141@hotmail.com>

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -11,25 +11,25 @@
  *
  * @author Bill Xue
  */
-class Mtce extends Application{
-    public function index()
-        {
-                $this->data['pagetitle'] = 'TODO List Maintenance';
-                $tasks = $this->tasks->all(); // get all the tasks
+class Mtce extends Application {
 
-               // substitute the status name
-foreach ($tasks as $task)
-        if (!empty($task->status))
-                                $task->status = $this->app->status($task->status);
+    public function index() {
+        $this->data['pagetitle'] = 'TODO List Maintenance';
+        $tasks = $this->tasks->all(); // get all the tasks
+        // substitute the status name
+        foreach ($tasks as $task)
+            if (!empty($task->status))
+                $task->status = $this->app->status($task->status);
 
 // build the task presentation output
-$result = '';   // start with an empty array        
-foreach ($tasks as $task)
-        $result .= $this->parser->parse('oneitem',(array)$task,true);
+        $result = '';   // start with an empty array        
+        foreach ($tasks as $task)
+            $result .= $this->parser->parse('oneitem', (array) $task, true);
 
 // and then pass them on
-$this->data['display_tasks'] = $result;
-$this->data['pagebody'] = 'itemlist';
-$this->render();
-        }
+        $this->data['display_tasks'] = $result;
+        $this->data['pagebody'] = 'itemlist';
+        $this->render();
+    }
+
 }
